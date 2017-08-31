d4853f50dde13a9e881ed20710dd60285ee7f94f
NativeDevlopment <creativeamar8@gmail.com>
2017-08-31 18:26:10 +0530

    json reponse make and annotation add

--- src/main/java/com/cricteam/controllers/OtpController.java
+++ src/main/java/com/cricteam/controllers/OtpController.java
@@ -1,30 +1,44 @@
 package com.cricteam.controllers;
 
+import org.apache.commons.httpclient.util.HttpURLConnection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cricteam.models.Otp;
+import com.cricteam.models.Response;
 import com.cricteam.service.OtpService;
 
 @Controller
 public class OtpController {
 	@Autowired
 	OtpService otpService;
-	 @RequestMapping("/sendOtp")
+	 @RequestMapping(value="/sendOtp",method = RequestMethod.GET, produces = "application/json")
 	  @ResponseBody
-	  public String create(String mobileNo) {
+	  public Response createOtp(String mobileNo) {
+		 
+		 Response response= new Response();
 	    Otp otp = null;
 	    String code="";
+	    if(mobileNo.length()>10){
 	    try {
-	      otp = new Otp(mobileNo,"9650",String.valueOf(System.currentTimeMillis()));
+	      otp = new Otp(mobileNo,"","");
 	      code = otpService.saveOtp(otp);
+	      otp.setOtp(code);
+	      response.statusCode=HttpURLConnection.HTTP_OK;
+	      response.message="Otp sucessfully sent to Mobile no";
+	      response.data=otp;
 	    }
 	    catch (Exception ex) {
-	      return "Error creating the user: " + ex.toString();
+	    	 response.statusCode=HttpURLConnection.HTTP_INTERNAL_ERROR;
+		      response.message="Internal server error";
+	    }}else{
+	    	 response.statusCode=HttpURLConnection.HTTP_LENGTH_REQUIRED;
+		      response.message="Please Enter Valid Mobile No.";
 	    }
-	    return "your otp code is"+ " "+code;
+	    return response;
 	  }
 
 }
